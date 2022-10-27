c0f5664bae59c42bc3b4725be931ee484702cc27
Harshil Savaj <savajharshil99@gmail.com>
2022-10-27 21:01:53 +0530

    array

---
+++ arrays.java
@@ -0,0 +1,14 @@
+public class arrays {
+
+    // public void arraydemo(int array[]){
+    //     System.out.println(array);
+    // }
+
+
+    public static void main(String[] args) {
+       int[] array={5,10,15,20,25,30};
+        System.out.println(array);
+      
+    }
+
+}
