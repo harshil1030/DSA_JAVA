bb120f9d7a3b0dd37fd15d5af0196aea14974c67
Harshil Savaj <savajharshil99@gmail.com>
2022-10-31 02:26:20 +0530

    Update hammingdistance.java

--- hammingdistance.java
+++ hammingdistance.java
@@ -1,43 +1,41 @@
 public class hammingdistance {
-    public static int hammingdista(String s1,String s2){
-        int i=0;
-        int count=0;
 
+
+    public static int hammingdista(String s1,String s2){    // Method for String 
+        int i=0,count=0;
         while(i<s1.length()){
             if(s1.charAt(i)!=s2.charAt(i)){
                 count++;
-             i++;
-            }           
+            }   
+            i++;        
         }
         return count;
-      
     }
 
-    public static int hamming(int[] arrA,int[] arrB){
+    public static int hamming(int[] arrA,int[] arrB){       //Method for Numeric Values.
         int i;
         int counta=0;
 
         for(i=0;i<arrA.length;i++){
             if(arrA[i]!=arrB[i]){
                 counta++;
-                i++;
-            }     
+            }  
+            i++;  
         }
         return counta;
     }
     
-    public static void main(String[] args) {
+    public static void main(String[] args) {                            //Main Method.
         String str1="hello";
         String str2="heelo";
 
-        hammingdista(str1,str2);
+        hammingdista(str1,str2);                                         //Calling Method1 
         System.out.println(hammingdista(str1, str2));
 
-        //int arrA[]={1,1,1,0,1,0,1};
-        int arrB[]={1,1,0,0,1,1,1};     
+        int arrA[]={1,1,0,1};
+        int arrB[]={1,1,0,1};     
 
-       // hamming(arrA, arrB);
-       // System.out.println(hamming(arrA, arrB));
-
+       hamming(arrA, arrB);                                             //Calling Method2
+       System.out.println(hamming(arrA, arrB));
     }
 }
