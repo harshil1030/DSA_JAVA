a4fefe93e26472e9a9597a3223a659b66bcd4a4f
Harshil Savaj <savajharshil99@gmail.com>
2022-10-31 22:31:55 +0530

    sortinganarray

---
+++ sortarray.java
@@ -0,0 +1,41 @@
+//Sorting an Array Medium Leetcode Array Problems..
+
+
+public class sortarray {
+
+    public static void sortarray(int[] arr){
+        int i,j;
+        int temp;
+
+        for(i=0;i<arr.length;i++){
+            for(j=i+1;j<arr.length;j++)
+            {
+                if(arr[i]>arr[j]){
+                    temp=arr[i];
+                    arr[i]=arr[j];
+                    arr[j]=temp;
+                }   
+            }
+        }
+        System.out.print("\nSorted Array   :");
+        for(i=0;i<arr.length;i++){
+            System.out.print(arr[i]+" ");
+        }
+    }
+
+
+
+
+
+    public static void main(String[] args) {
+        int[] myarray={5,4,3,2,1};
+        int i;
+        System.out.print("Original Array :");
+        for(i=0;i<myarray.length;i++){
+            System.out.print(myarray[i]+" ");
+        }
+
+        sortarray(myarray);
+        
+    }
+}
