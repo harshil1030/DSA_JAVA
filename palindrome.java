c964a136e6989e3aaa7ddb02f790c8d45fe070f8
Harshil Savaj <savajharshil99@gmail.com>
2022-11-08 21:13:00 +0530

    palindrome
    To check whether the given string is palindrome or not.

---
+++ palindrome.java
@@ -0,0 +1,21 @@
+// Check wheather the String is Palindrome or not.
+// How to check the string is Palindrome or not > i.e;
+// If :  Before Palindrome : madam
+//       After Palindrome  : madam
+// i.e  121,dad,mom those are palindrome strings .
+// so write a program which shows you ; if string is palindrome then return true ;
+// else return False ;
+
+
+public class palindrome {
+
+
+
+
+
+
+    public static void main(String[] args) {
+        
+
+    }
+}
