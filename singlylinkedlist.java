926eec09f01e17fcd9ea648c3452da65ed72b4db
Harshil Savaj <savajharshil99@gmail.com>
2022-11-11 12:32:09 +0530

    single linked list

---
+++ singlylinkedlist.java
@@ -0,0 +1,11 @@
+//Lecture no: 35 
+//Topic: Represent a Singly linked List in java.
+//there are total 3 types of Linked List 
+//1.Sinly Linked List 2.Doubly linked list 3.Circular Linked List
+
+public class singlylinkedlist {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+
+    }
+}
