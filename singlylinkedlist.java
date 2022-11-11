25c221ab6613dc2325fb4fa15d6de48dbb90f5d5
Harshil Savaj <savajharshil99@gmail.com>
2022-11-11 13:40:18 +0530

    linked list

--- singlylinkedlist.java
+++ singlylinkedlist.java
@@ -4,8 +4,27 @@
 //1.Sinly Linked List 2.Doubly linked list 3.Circular Linked List
 
 public class singlylinkedlist {
-    public static void main(String[] args) {
-        System.out.println("Hello World");
+    private ListNode head;    //instance variable of type listnode //holding Linked List
+    private static class ListNode{
+        private int data; //generic type
+        private ListNode next;
 
+        public ListNode(int data){
+            this.data=data;
+            this.next=null;
+        } 
+    }
+    public static void main(String[] args) {
+        singlylinkedlist sll = new singlylinkedlist();
+        sll.head=new ListNode(10);
+        ListNode second=new ListNode(1);
+        ListNode third=new ListNode(8);
+        ListNode fourth=new ListNode(11);
+
+        //ne we will connect them to form a Chain
+
+       sll.head.next=second; //10-->1 
+       second.next=third;    //10-->1-->8
+       third.next=fourth;    //10-->1-->8-->11
     }
 }
