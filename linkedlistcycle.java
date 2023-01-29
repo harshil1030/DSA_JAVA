d90728998a02625dcdf7cfbf51c4d2772eec04e7
Harshil Savaj <savajharshil99@gmail.com>
2023-01-29 12:22:00 +0530

    initial

--- linkedlistcycle.java
+++ linkedlistcycle.java
@@ -18,8 +18,25 @@ public class linkedlistcycle {
             current=current.next;
         }
         System.out.println("null");
-    }
+    } 
 
+    
+
+    public void hascycle(ListNode head){
+        ListNode fast=head;
+        ListNode slow=head;
+        while(fast != null && fast.next != null){ //No Loop as both of them are Null but if  equal then return true;
+            slow = slow.next;
+            fast = fast.next.next;
+ 
+            if(slow == fast)
+                System.out.println("True : There is a Cycle in Linked List ");
+                //return true;
+          
+        }
+            System.out.println("false : There is No Cycle in Linked List");  
+        //return false;   // False if there is no Cycle. 
+    }
     // public void insertlast(int value){
     //     ListNode newNode=new ListNode(value); //creating new node and pass the value in the 
     //     // constructor of the listnode
@@ -40,8 +57,22 @@ public class linkedlistcycle {
 
     public static void main(String[] args) {
         linkedlistcycle sll =new linkedlistcycle();
-        
-        
+        sll.head=new ListNode(10);
+        ListNode second=new ListNode(1);
+        ListNode third=new ListNode(8);
+        ListNode fourth=new ListNode(11);
+        ListNode fifth=new ListNode(15);
+        ListNode sixth=new ListNode(19);
+
+        sll.head.next=second; //10-->1 
+       second.next=third;    //10-->1-->8
+       third.next=fourth;    //10-->1-->8-->11
+       fourth.next=fifth;
+       fifth.next=sixth;
+       sixth.next=null;      
+       
+       
         sll.display();
+        sll.hascycle(sll.head);
     }
 }
