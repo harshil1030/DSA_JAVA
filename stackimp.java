f6e24484b2ae564bd00d840b4d719830a8ed48e3
Harshil Savaj <savajharshil99@gmail.com>
2023-03-29 21:44:52 +0530

    stack_initial

---
+++ stackimp.java
@@ -0,0 +1,17 @@
+import java.util.Stack;
+
+public class stackimp {
+
+
+    public static void main(String[] args) {
+        Stack<Integer> s=new Stack<Integer>();
+        s.push(10);
+        s.push(20);
+        s.push(30);
+        s.push(40);
+        s.push(50);
+        System.out.println("The Stack is : "+ s);
+
+        
+    }
+}
