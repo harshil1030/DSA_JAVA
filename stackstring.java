f8c8b83df0ab2ac4de6cae5db4dcc4b4dfd42106
Harshil Savaj <savajharshil99@gmail.com>
2023-05-31 11:26:40 +0530

    strings in stack

--- stackstring.java
+++ stackstring.java
@@ -5,7 +5,16 @@ public class stackstring {
 
 
     public static void main(String[] args) {
-        Stack games=new Stack();
-        S
+        Stack<String> games=new Stack<String>();
+        games.add("Call of Duty");
+        games.add("Super Mario");
+        games.add("Valorent");
+
+        // System.out.println(games.pop());
+        // System.out.println(games.pop());
+
+        System.out.println(games.peek());
+        System.out.println(games);
+        
     }
 }
